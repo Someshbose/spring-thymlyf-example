d2a836cf64544fe5f61daf03ec433297b4103ecc
Someshbose <iamsomeshbose@gmail.com>
2021-07-28 16:56:54 +0530

    initial changes

---
+++ src/main/java/someshbose/github/io/springthymlyfexample/SpringThymlyfExampleApplication.java
@@ -0,0 +1,13 @@
+package someshbose.github.io.springthymlyfexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymlyfExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymlyfExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/someshbose/github/io/springthymlyfexample/SpringThymlyfExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package someshbose.github.io.springthymlyfexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymlyfExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
